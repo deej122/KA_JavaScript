3054cc6ebc2ae716b415a27b9f0eb6ae6f3e7f13
DJ Jagannathan <desjag@gmail.com>
2014-08-19 17:06:45 -0400

    errors working but a little buggy (need to consistently update model

--- app/coffee_scripts/app.js
+++ app/coffee_scripts/app.js
@@ -10,18 +10,72 @@
   }).controller('MainController', function($scope, $http) {
     $scope.profile = false;
     $scope.jsCode = null;
-    $scope.parseIt = function() {
+    return $scope.parseIt = function() {
       $scope.body = {
         jsCode: $scope.jsCode
       };
       console.log($scope.jsCode);
       console.log(typeof $scope.jsCode);
       return $http.post('/api/controller/parse', $scope.body).success(function(parsed) {
-        console.log(parsed);
+        var containsFor, containsIf, containsVariable, declaration, _i, _len, _ref;
+        console.log(parsed.ast.body);
         $scope.parsedCode = parsed;
+        _ref = $scope.parsedCode.ast.body;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          declaration = _ref[_i];
+          if ($scope.needVariable === true) {
+            if (declaration.type === "VariableDeclaration") {
+              containsVariable = true;
+              $scope.variableError = null;
+            } else {
+              containsVariable = false;
+              $scope.variableError = "You must declare a variable in your code!";
+            }
+          } else if ($scope.needIf === true) {
+            if (declaration.type = "IfStatement") {
+              containsIf = true;
+              $scope.ifError = null;
+            } else {
+              containsIf = false;
+              $scope.ifError = "You must use an if statement in your code!";
+            }
+          } else if ($scope.needFor === true) {
+            if (declaration.type = "ForStatement") {
+              containsFor = true;
+              $scope.forError = null;
+            } else {
+              containsFor = false;
+              $scope.forError = "You must use a for loop in your code!";
+            }
+          }
+          if ($scope.noVariable === true) {
+            if (declaration.type === "VariableDeclaration") {
+              containsVariable = true;
+              $scope.variableError = "You must not declare any variables in your code!";
+            } else {
+              containsVariable = false;
+              $scope.variableError = null;
+            }
+          } else if ($scope.noIf === true) {
+            if (declaration.type = "IfStatement") {
+              containsIf = true;
+              $scope.ifError = "You must not use an if statement in your code!";
+            } else {
+              containsIf = false;
+              $scope.ifError = null;
+            }
+          } else if ($scope.noFor === true) {
+            if (declaration.type = "ForStatement") {
+              containsFor = true;
+              $scope.forError = null;
+            } else {
+              containsFor = false;
+              $scope.forError = null;
+            }
+          }
+        }
       });
     };
-    return console.log($scope.profile);
   });
 
 }).call(this);
--- app/scripts/app.coffee
+++ app/scripts/app.coffee
@@ -22,7 +22,55 @@ angular.module('kajsApp', [
       $http.post(
         '/api/controller/parse', $scope.body
       ).success (parsed) ->
-        console.log parsed
+        console.log parsed.ast.body
         $scope.parsedCode = parsed
+        for declaration in $scope.parsedCode.ast.body
+            if $scope.needVariable == true
+              if declaration.type == "VariableDeclaration"
+                containsVariable = true
+                $scope.variableError = null
+              else
+                containsVariable = false
+                $scope.variableError = "You must declare a variable in your code!"
+                
+            else if $scope.needIf == true
+              if declaration.type = "IfStatement"
+                containsIf = true
+                $scope.ifError = null
+              else
+                containsIf = false
+                $scope.ifError = "You must use an if statement in your code!"
+
+            else if $scope.needFor == true
+              if declaration.type = "ForStatement"
+                containsFor = true
+                $scope.forError = null
+              else
+                containsFor = false
+                $scope.forError = "You must use a for loop in your code!"
+
+
+            if $scope.noVariable == true
+              if declaration.type == "VariableDeclaration"
+                containsVariable = true
+                $scope.variableError = "You must not declare any variables in your code!"
+              else
+                containsVariable = false
+                $scope.variableError = null
+
+            else if $scope.noIf == true
+              if declaration.type = "IfStatement"
+                containsIf = true
+                $scope.ifError = "You must not use an if statement in your code!"
+              else
+                containsIf = false
+                $scope.ifError = null
+
+            else if $scope.noFor == true
+              if declaration.type = "ForStatement"
+                containsFor = true
+                $scope.forError = null
+              else
+                containsFor = false
+                $scope.forError = null
         return
-    console.log $scope.profile
